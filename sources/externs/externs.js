423fd35ffbfd9ebe074959d8cacbeea5e3667995
Grzegorz Tomicki <gtomik@gmail.com>
2020-05-03 09:56:20 +0200

    closure-compiler

---
+++ sources/externs/externs.js
@@ -0,0 +1,12 @@
+var options = {};
+
+options.element;
+options.padding;
+options.zIndex;
+options.animationTiem;
+options.cursor;
+options.cursor.cursorIn;
+options.cursor.cursorOut;
+options.overlay;
+options.overlay.color;
+options.overlay.opacity;
