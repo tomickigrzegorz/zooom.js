744a7470a190bd4205c40605216ce8093945bed0
Grzegorz Tomicki <gtomik@gmail.com>
2020-08-25 20:34:31 +0200

    small improvements

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,30 @@
 import copy from 'rollup-plugin-copy';
-import serve from 'rollup-plugin-serve';
-import livereload from 'rollup-plugin-livereload';
 import banner from 'rollup-plugin-banner';
-import compiler from '@ampproject/rollup-plugin-closure-compiler';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import pkg from "./package.json";
 
 const { PRODUCTION } = process.env;
 
 export default {
   input: 'sources/script.js',
   output: {
-    file: 'docs/zooom.min.js',
+    file: pkg.main,
     format: 'iife',
     name: 'Zooom',
-    sourcemap: PRODUCTION ? false : true
+    sourcemap: !PRODUCTION
   },
   plugins: [
-    compiler({
-      language_in: 'ECMASCRIPT6',
-      language_out: 'ECMASCRIPT5',
-      externs: './sources/externs/externs.js',
-      compilation_level: 'ADVANCED',
-      // compilation_level: 'WHITESPACE_ONLY',
-      // compilation_level: 'SIMPLE',
-      // debug: true,
-      // source_map_format: 'V3'
+    babel({
+      exclude: 'node_modules/**',
+      babelHelpers: 'bundled'
     }),
+    terser(),
     (PRODUCTION && banner('Zooom.js - the easiest way to enlarge a photo\n@version v<%= pkg.version %>\n@link <%= pkg.homepage %>\n@license <%= pkg.license %>')),
     copy({
       targets: [
         { src: './images/**/*', dest: 'docs/images' }
       ]
-    }),
-    (!PRODUCTION && serve({ open: true, contentBase: ['./docs', './sources'] })),
-    (!PRODUCTION && livereload())
+    })
   ]
 };
