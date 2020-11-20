50286417ca1705749638f3c5e0a9e762507d468b
Grzegorz Tomicki <gtomik@gmail.com>
2020-11-20 16:35:14 +0100

    refactor: rem plugin banner, terser on prod, umd

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import copy from 'rollup-plugin-copy';
-import banner from 'rollup-plugin-banner';
 import babel from '@rollup/plugin-babel';
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
@@ -10,15 +9,10 @@ import pkg from "./package.json";
 const { PRODUCTION } = process.env;
 const extensions = ['.ts'];
 
-export default {
-  input: 'sources/js/index.ts',
-  output: {
-    file: pkg.main,
-    format: 'iife',
-    name: 'Zooom',
-    sourcemap: !PRODUCTION
-  },
-  plugins: [
+const banner = `/*!\n* Zooom.js - the easiest way to enlarge a photo\n* @version v${pkg.version}\n* @link ${pkg.homepage}\n* @license ${pkg.license}\n*/`;
+
+const plugins = () => {
+  return [
     babel({
       babelrc: false,
       exclude: 'node_modules/**',
@@ -27,7 +21,7 @@ export default {
       plugins: ['@babel/proposal-class-properties'],
       extensions
     }),
-    terser(),
+    PRODUCTION && terser(),
     typescript({
       noEmitOnError: false
     }),
@@ -36,8 +30,31 @@ export default {
         { src: './images/**/*', dest: 'docs/images' }
       ]
     }),
-    PRODUCTION && banner('Zooom.js - the easiest way to enlarge a photo\n@version v<%= pkg.version %>\n@link <%= pkg.homepage %>\n@license <%= pkg.license %>'),
     !PRODUCTION && serve({ open: true, contentBase: 'docs' }),
     !PRODUCTION && livereload(),
   ]
-};
+}
+
+export default [
+  {
+    input: 'sources/js/index.ts',
+    output: {
+      file: pkg.main,
+      format: 'iife',
+      name: 'Zooom',
+      banner,
+      sourcemap: !PRODUCTION
+    },
+    plugins: plugins()
+  },
+  {
+    input: 'sources/js/index.ts',
+    output: {
+      file: pkg.browser,
+      format: 'umd',
+      name: 'Zooom',
+      banner,
+    },
+    plugins: plugins()
+  },
+];
