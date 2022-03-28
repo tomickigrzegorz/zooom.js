13aa8ae48dbff253dc6647f1a7edbd195006a30b
Grzegorz Tomicki <gtomik@gmail.com>
2022-03-28 16:40:42 +0200

    fix: Inline styles are removed #41

--- dist/zooom.es.js
+++ dist/zooom.es.js
@@ -107,8 +107,8 @@ class Zooom {
         /**
          * @method loadImage - cload image if data-zooom-big is set
          *
-         * @param {HTMLImageElement} - target
-         * @param {String} - bigImage
+         * @param {HTMLImageElement} target
+         * @param {String} bigImage
          */
         this._loadImage = (target, bigImage) => {
             return new Promise((resolve, reject) => {
@@ -198,7 +198,7 @@ class Zooom {
             img.style.transform = `matrix(${scale},0,0,${scale},${X},${Y})`;
             // hide orginal image
             setTimeout(() => {
-                this._imageZooom.style.visibility = "hidden";
+                this._imageZooom.style.setProperty("visibility", "hidden");
             }, 50);
             // remove image
             img.addEventListener("click", this._reset);
@@ -211,7 +211,7 @@ class Zooom {
             setTimeout(() => {
                 var _a;
                 (_a = this._clonedImg.parentNode) === null || _a === void 0 ? void 0 : _a.removeChild(this._clonedImg);
-                this._imageZooom.removeAttribute("style");
+                this._imageZooom.style.removeProperty("visibility");
             }, this._animTime);
         };
         this._element = className;
